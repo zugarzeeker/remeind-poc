8d8d1f688f9c106f362b30abda413c4c2b6f3038
Supanut Apikulvanich <supanut.overload@gmail.com>
2017-02-10 18:03:18 +0700

    :hankey: Delete Lambda

--- delete-lambda/index.js
+++ delete-lambda/index.js
@@ -1,10 +1,10 @@
 var AWS = require('aws-sdk');
-var lambda = new AWS.Lambda();
+var config = require('config');
+var lambda = new AWS.Lambda(config);
 var params = {
-  FunctionName: "myFunction",
-  Qualifier: "1"
- };
- lambda.deleteFunction(params, function(err, data) {
-   if (err) console.log(err, err.stack); // an error occurred
-   else     console.log(data);           // successful response
- });
+  FunctionName: "hello-lambda-8"
+};
+lambda.deleteFunction(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
