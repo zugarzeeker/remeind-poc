744735f4ef644ab6d8abfec993eae8109ec559de
Supanut Apikulvanich <supanut.overload@gmail.com>
2017-02-10 18:09:59 +0700

    :hankey: Lambda subscribe SNS

---
+++ lambda-subscribe-sns/index.js
@@ -0,0 +1,14 @@
+var AWS = require('aws-sdk');
+var config = require('config')
+var sns = new AWS.SNS(config);
+
+var functionName = 'hello-lambda-6'
+var params = {
+  Protocol: 'lambda',
+  TopicArn: config.TopicArn,
+  Endpoint: config.prefixFunctionArn + functionName
+};
+sns.subscribe(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
