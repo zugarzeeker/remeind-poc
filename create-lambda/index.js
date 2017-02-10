069f3dfaddbcb886a86ce5f347583d5f0a98c71f
Supanut Apikulvanich <supanut.overload@gmail.com>
2017-02-10 18:03:04 +0700

    :hankey: Create Lambda

--- create-lambda/index.js
+++ create-lambda/index.js
@@ -1,33 +1,31 @@
 // http://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html
+var functionName = 'hello-lambda-9'
 var AWS = require('aws-sdk');
-var lambda = new AWS.Lambda();
+var config = require('config')
+var awsConfig = new AWS.Config(config)
+var lambda = new AWS.Lambda(awsConfig);
 var params = {
   Code: { /* required */
-    S3Bucket: 'STRING_VALUE',
-    S3Key: 'STRING_VALUE',
-    S3ObjectVersion: 'STRING_VALUE',
-    ZipFile: new Buffer('...') || 'STRING_VALUE'
+    S3Bucket: 'hello-world-1-2-3', // BUCKET
+    S3Key: 'code.zip' // FILE_NAME
   },
-  FunctionName: 'STRING_VALUE', /* required */
-  Handler: 'STRING_VALUE', /* required */
-  Role: 'STRING_VALUE', /* required */
-  Runtime: 'nodejs', /* required */
-  DeadLetterConfig: {
-    TargetArn: 'STRING_VALUE' // SNS Topic
-  },
-  Description: 'Dose Reminder', // some conventions ?
+  FunctionName: functionName, /* required */
+  Handler: 'index.handler', /* required */
+  Role: config.Role, /* required */
+  Runtime: 'nodejs4.3', /* required */
+  Description: 'Reminder', // some conventions ?
   Environment: {
     Variables: {
       someKey: 'STRING_VALUE',
       /* anotherKey: ... */
     }
   },
-  Version: 'STRING',
   MemorySize: 128,
   Publish: true || false,
   Timeout: 3, // default is 3 sec
   Publish: true // ?
 };
+
 lambda.createFunction(params, function(err, data) {
   if (err) console.log(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
