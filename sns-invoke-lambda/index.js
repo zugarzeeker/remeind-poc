bee3daec1453c35b1e4714e8aff8464d00cd789f
Supanut Apikulvanich <supanut.overload@gmail.com>
2017-02-10 18:30:28 +0700

    :hankey: Sns invoke Lambda

---
+++ sns-invoke-lambda/index.js
@@ -0,0 +1,8 @@
+exports.handler = (event, context, callback) => {
+  try {
+    console.log(event.Records[0].Sns);
+  } catch (err) {
+    console.log(err);
+  }
+  callback(null, 'Hello from Lambda');
+};
